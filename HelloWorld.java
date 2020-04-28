3a1a7556cbe440c49d764bbe96053c651331ea10
git-rinki <satishchandrasah1989@gmail.com>
2020-04-28 11:52:32 +0530

    Test Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {;;
+	public static void main(String[] args) {
 
 
-		for(int i=1;i<=10;i++){dddjdj;
+		for(int i=1;i<=10;i++){sfdsf!
 			
 		System.out.println("Hello World..!! --" +i);
 		}
